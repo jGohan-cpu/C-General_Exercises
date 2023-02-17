7ece97dfdf5365c59a68fe190a66b1d52be7f751
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-17 23:32:55 +0000

    6 exercise second try

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 /**
+ *Main - Entry point
  *
+ *Description - Prints Size of every varaible
  *
- *
- *
- *
+ *Return - Always Success (0)
  */
 
 int main(void)
@@ -16,11 +16,11 @@ int main(void)
 	long long d;
 	float e;
 
-	printf("Size of a char: %ld byte(s) \n", sizeof(a));
-	printf("Size of an int: %ld byte(s)\n", sizeof(b));
-	printf("Size of a long int: %ld  byte(s)\n", sizeof(c) );
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
-	printf("Size of a float: %ld byte(s)\n", sizeof(e));
+	printf("Size of a char: %ld byte(s) \n", (unsigned long) sizeof(a));
+	printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %ld  byte(s)\n", (unsigned long)sizeof(c) );
+	printf("Size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(e));
 
 	return (0);
 }	
