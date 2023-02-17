4b4d84228f9c399838ff7fccaa47ce8e354a1d1d
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-17 16:52:29 +0000

    4 exercise first try

---
+++ hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: prints strings
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);		
+
+}
