8c583f612d2df8dcce971f017fd413a07700a914
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-20 21:22:34 +0000

    betty 9 exercise

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
 /**
  *main - Entry point
  *
- *Return: Always 0 
+ *Return: Always 0 (Success)
  */
 
 int main(void)
@@ -15,15 +15,15 @@ int main(void)
 	{
 	putchar(i);
 
-		if(i != '9')
+		if (i != '9')
 		{
 		putchar(',');
 		putchar(' ');
-		}	
+		}
 	}
 
 	putchar('\n');
 
-	return(0);
+	return (0);
 
 }
