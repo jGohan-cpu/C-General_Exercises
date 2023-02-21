9d050f2ef2a376eb2c0adf4a1132fc6ed9ef2590
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-21 14:58:47 +0000

    100 exercise

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always Success (0)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for(i = '0'; i <= '9'; i++)
+	{
+		for(j = '0'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(',');
+			putchar(' ');	
+		}
+	}
+	putchar('\n');
+
+	return(0);
+}	
