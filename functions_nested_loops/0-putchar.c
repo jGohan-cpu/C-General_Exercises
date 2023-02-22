9458ec5b73a9702390c9e18e60ac6164ce20226c
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-22 17:13:13 +0000

    2 exercise first try

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 
- /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
