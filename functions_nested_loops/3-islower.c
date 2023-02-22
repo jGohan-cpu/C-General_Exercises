f8c61c368875247d07ced8c5dc0a9b8007fc3c4b
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-22 18:30:44 +0000

    4 exercise

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * _islower - checks if character is lower case
+ *
+ * Return: Always 0 (Success)
  */
 
 int _islower(int c)
 {
 
-	if(c >= 97 && c <= 122)
+
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*
+*/
+
+int _isalpha(int c)
+{
+
+	if(c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	if(c >= 'A' && c<= 'Z')
+	{
+		return (1);
+	}
+	
+	return(0);
+}
