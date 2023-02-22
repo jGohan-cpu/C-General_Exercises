b1d9dcf1909748e361cf85fada87dc5f73b48656
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-22 19:04:21 +0000

    3 exercise

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -12,15 +12,15 @@
 int _isalpha(int c)
 {
 
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
 
-	if(c >= 'A' && c<= 'Z')
+	if (c >= 'A' && c<= 'Z')
 	{
 		return (1);
 	}
 
-	return(0);
+	return (0);
 }
