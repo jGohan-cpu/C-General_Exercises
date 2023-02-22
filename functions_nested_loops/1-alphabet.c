ee3cee60f64d2543febd775c5c3c8028fc8d6c06
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-22 12:49:32 +0000

    1 exercise first try

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+
+	putchar('\n');
+}
