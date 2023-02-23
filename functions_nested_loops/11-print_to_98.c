e10aac40f80c5351cd3ca1c96e7995cff9534e8f
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-23 21:16:32 +0000

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,17 +11,33 @@ void print_to_98(int n)
 {
 	int i;
 
-	if(n => 98)
+	if(n >= 98)
 	{
 		for(i = n; i >= 98; i--)
 		{
-		printf("%d, ", i);
+		printf("%d ", i);
+
+		if(i != 98)
+		{
+		putchar(',');
+		putchar(' ');
 		}
+		}
+		putchar('\n');
+	}
 
 	else
 	{
 	for(i = n; i <= 98; i++)
 	{
-		printf("%d, ", i);
+		printf("%d", i);
+
+		if(i != 98)
+		{
+		putchar(',');
+                putchar(' ');
+		}
+	}
+	putchar('\n');
 	}
 }
