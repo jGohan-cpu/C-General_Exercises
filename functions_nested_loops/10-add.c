30b4b1103e0766719b64e4e84f212366f47d33a4
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-23 13:19:47 +0000

    10 exercise first try

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int add -
+ * @a:
+ * @b:
+ * @c:
+ */
+
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return(c);
+}
---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_last_digit - prints last digit of number
+ *@c: receives int
+ *Return: returns int
+ */
+
+int print_last_digit(int c)
+{
+
+	int last = c % 10;
+
+	return (last);
+}
