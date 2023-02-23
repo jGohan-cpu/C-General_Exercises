dd38be182125e4e956e2fa867d20644fb2ac4c35
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-23 19:32:28 +0000

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,28 +10,17 @@ void jack_bauer(void)
 {
 	int a;
 	int b;
-	int c;
-	int d;
 
-	for(a = '0'; a <= '2'; a++)
+	for(a = 0; a <= 23; a++)
 	{
-		for(b = '0'; b <= '4'; b++ )
+		for(b = 0; b <= 59; b++)
 		{
-				for(c = '0'; c <= '5'; c++)
-				{
-					for(d = '0'; d <= '9'; d++)
-					if (a <= '2' && b <= '' )
-					{	
-					{
-						putchar(a);
-						putchar(b);
-						putchar(':');
-						putchar(c);
-						putchar(d);
-						putchar('\n');
-					}
-					}
-				}
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
 		}
-}
+	}
 }
