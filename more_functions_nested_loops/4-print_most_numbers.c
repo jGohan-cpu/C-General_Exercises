35b9e93da0e4a041f6480d9d9db34aeb46efa3ee
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-24 15:05:52 +0000

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_most_numbers - print number
+ */
+
+void print_most_numbers(void)
+{
+
+	int i;
+
+	for(i = '0'; i <= '9'; i++)
+	{
+	putchar(i);
+	}
+
+	putchar('\n');
+
+}
