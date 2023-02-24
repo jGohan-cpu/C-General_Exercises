42bcbdb6d0bb887aeaba0b6691a9c8e1738ad34a
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-24 14:41:02 +0000

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	putchar(i + '0');
+	}
+
+	putchar('\n');
+}
