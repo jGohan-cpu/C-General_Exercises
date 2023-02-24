8a82b7759b3b20c94235628c41fba3bb6ebd078c
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-24 17:32:41 +0000

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *more_number - print numbers ten times
+ */
+
+void more_numbers(void)
+{
+
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+
+			if (j >= 10)
+			{
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+			}
+
+			else
+			{
+				putchar(j + '0');
+			}
+		}
+		putchar('\n');
+	}
+}
