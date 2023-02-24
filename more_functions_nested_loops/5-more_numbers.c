b7866ccca4e98a7afbf4fb1d30f752b2b1abe042
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-24 17:37:00 +0000

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -19,13 +19,8 @@ void more_numbers(void)
 			if (j >= 10)
 			{
 				putchar((j / 10) + '0');
+			}
 				putchar((j % 10) + '0');
-			}
-
-			else
-			{
-				putchar(j + '0');
-			}
 		}
 		putchar('\n');
 	}
