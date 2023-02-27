eb96a8c38600d6e9ca93f2eed88be8f7358873f5
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-27 21:36:37 +0000

    100

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - prints largest prime number
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	int i;
+	long j;
+	long num = 612852475143;
+	double square = sqrt(num);
+
+	for (i = 1; i <= ; i++)
+	{
+		if (num % x == 0)
+		{
+			j = num / i;
+		}
+	}
+	
+	printf("%ld\n", j);
+
+	return (0);
+}
