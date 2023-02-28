6202a9b2747fa711645eef9357fda073f4db05c8
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-28 15:12:34 +0000

    4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,27 @@
 #include <string.h>
 
 /**
- *
- *
+ * print_rev - prints reversed string
+ * @s: receives pointer
  */
 
 void print_rev(char *s)
 {
 
 	int i;
-	int temp;
-	int len = strlen(s);
-	int middle = len / 2;
+	int j;
+	int a = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	a++;
+	}
+
+	for (j = (a - 1); j >= 0; j--)
+	{
+	_putchar(s[j]);
+	}
+
+	_putchar('\n');
 
 }
