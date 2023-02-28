110a9ef558b727b18b205c09240c45870dd213f5
jGohan-cpu <jjoseriveram01@gmail.com>
2023-02-28 17:25:36 +0000

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - half of string
+ * @str: receives 
+ */
+
+void puts_half(char *str)
+{
+
+	int i;
+	int len = strlen(str);
+	int half = len / 2;
+
+	for (i = half; str[i] != '\0'; i++)
+	{
+			putchar(str[i]);
+	}
+
+	putchar('\n');
+}
