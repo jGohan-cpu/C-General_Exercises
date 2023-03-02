322b5f823bc606e2cc13ba0e1fb93a12553999f6
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-02 01:56:39 +0000

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _strcat - concatenates strings
+ * @dest: receives char
+ * @src: receives char
+ * Return: returns char
  */
 
 char *_strcat(char *dest, char *src)
@@ -13,14 +13,14 @@ char *_strcat(char *dest, char *src)
 
 	int i,j;
 
-	for(i = 0; dest[i] != '\0'; i++);
+	for (i = 0; dest[i] != '\0'; i++);
 
 	for (j = 0; src[j] != '\0'; j++)
 	{
-	dest[i + j]=src[j];
+	dest[i + j] = src[j];
 	}
 
-	dest[i + j]='\0';
+	dest[i + j] = '\0';
 
 	return (dest);
 }
