aeb04eb58269e8c7d959e824149bbe08b00c3e2e
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-03 12:54:33 +0000

    101

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -7,24 +7,28 @@
 
 int main(void)
 {
-	int i, j;
+	int i, j, k;
 
-	 for (i = 0; i < 9; i++)
-    	{
-        	for (j = i + 1; j < 10; j++)
-        	{
-            		
-			putchar((i % 10) + '0');
-            		putchar((j % 10) + '0');
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for(k = j + 1; k < 10; k++)
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((k % 10) + '0');
 
-            	if (i == 8 && j == 9)
-			continue;
-            		putchar(',');
-            		putchar(' ');
-        	}
-    	}
+				if (i == 7 && j == 8 && k == 9)
+					continue; 
 
-    putchar('\n');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-    return (0);
+	putchar('\n');
+
+	return(0);
 }
