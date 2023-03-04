8469157cc00fbaf1898a7b4c38f81d2b56a1b685
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-04 22:06:10 +0000

    3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,3 +10,26 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int a = 0;
+	int b;
+	unsigned int c = 0;
+
+	while (accept[a])
+	{
+		b = 0;
+
+		while (s[b] != 32)
+		{
+			if (accept[a] == s[b])
+			{
+				c++;
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return (c);
+}
