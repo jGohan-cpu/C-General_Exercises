88d831a1749a369d33d783769ab1b63e8b062544
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-07 15:22:28 +0000

    3

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - raised to the power
+ * @x: value
+ * @y:power
+ * Return: integer
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+
+
+}
