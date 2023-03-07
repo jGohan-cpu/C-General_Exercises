5a5ccff1c6993db5949ab42cc9957dc042db80be
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-07 11:36:08 +0000

    0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints in recursion
+ * @s: receives pointer
+ */
+
+void _puts_recursion(char *s)
+{
+
+	if (*s)
+	{
+	putchar(*s);
+	_puts_recursion(s + 1);
+	}
+
+	else
+	{
+	putchar('\n');
+	}
+
+}
