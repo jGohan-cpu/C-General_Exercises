119adaf42d7b80ce82fac31eb2b3ccabdac81a67
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-07 13:27:26 +0000

    2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -10,16 +10,14 @@
 int _strlen_recursion(char *s)
 {
 
-	int i = 0;
-
-	if(*s != '\0')
+	if(*s == '\0')
 	{
-	_strlen_recursion(s + 1);
+	return (0);
 	}
 
 	else
 	{
-	return (1);
+	return 1 + _strlen_recursion(s + 1);
 	}
 
 }
