2a66f2c2a6acf937f56361e58f7f5a5fe4406980
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-09 19:37:27 +0000

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: integer
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+
+}
