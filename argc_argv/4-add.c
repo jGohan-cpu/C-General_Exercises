6dbcf3dca96dabf88d6f6f1eea35f56f94f7046e
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-10 16:02:46 +0000

    4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,25 +9,26 @@
 
 int main(int argc, char *argv[])
 {
+	int i, j, sum;
 
-	int i, sum;
-
-	if (argc == 1)
-	{
-	printf("0\n");
-	return (0);
-	}
-
-	else
-	{
-	if (argc > 1)
-	{
 	for (i = 1; i < argc; i++)
 	{
+	for (j = 0; argv[i][j]; j++)
+	{
+	if (argv[i][j] < '0' || argv[i][j] > '9')
+	{
+
+	printf("Error\n");
+	return (1);
+
+	}
+	}
+
 	sum += atoi(argv[i]);
+
 	}
-	}
+
 	printf("%d\n", sum);
+
 	return (0);
-	}
 }
