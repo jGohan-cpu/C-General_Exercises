ab6136e847057d06b5ae398a0bb7406c33a69aad
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-13 18:47:43 +0000

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,7 +16,6 @@ char *str_concat(char *s1, char *s2)
 	int len1 = strlen(s1);
 	int len2 = strlen(s2);
 	int i;
-	int j;
 	char *cat = malloc(len1 + len2 + 1);
 
 	if (s1 == NULL || s2 == NULL)
@@ -34,12 +33,12 @@ char *str_concat(char *s1, char *s2)
 	cat[i] = s1[i];
 	}
 
-	for (j = 0; j < len2; j++)
+	for (i = 0; i < len2; i++)
 	{
-	cat[i + j] = s2[j];
+	cat[len1 + i] = s2[i];
 	}
 
-	cat[i + j] = '\0';
+	cat[len1 + len2] = '\0';
 
 	return (cat);
 }
