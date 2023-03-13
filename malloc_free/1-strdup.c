c662ab10405ede539e227014c396ae7097b22fe6
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-13 15:07:23 +0000

    1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_strdup - duplicates strings
+ * @str: character
+ * Return: char
+ */
+
+char *_strdup(char *str)
+{
+	int i;	
+	int len = strlen(str) + 1;
+	char *dup = malloc(len);
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+
+	for (i = 0; i < len ; i++)
+	{
+	dup[i] = str[i];
+	}
+
+	return (dup);
+}
