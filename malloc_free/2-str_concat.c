0e8e382881603496cb15c7512a4d9ec64d1d659a
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-13 18:43:40 +0000

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  *str_concat - concatenates strings
@@ -12,3 +13,33 @@
 char *str_concat(char *s1, char *s2)
 {
 
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+	int i;
+	int j;
+	char *cat = malloc(len1 + len2 + 1);
+
+	if (s1 == NULL || s2 == NULL)
+        {
+        return (NULL);
+        }
+
+	if (cat == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len1; i++)
+	{
+	cat[i] = s1[i];
+	}
+
+	for (j = 0; j < len2; j++)
+	{
+	cat[i + j] = s2[j];
+	}
+
+	cat[i + j] = '\0';
+
+	return (cat);
+}
