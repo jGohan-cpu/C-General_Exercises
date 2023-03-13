1d2a56650a8c62718e0f5e66e0eff1bc00f533a5
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-13 14:39:08 +0000

    0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *create array - creates array
+ *@size: size
+ *@c: initializer
+ *Return: character
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+	unsigned int i;
+	char *arr = malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+	return (NULL);
+	}
+
+	for (i = 0; i < size ; i++)
+	{
+	arr[i] = c;
+	}
+
+	return (arr);
+}
