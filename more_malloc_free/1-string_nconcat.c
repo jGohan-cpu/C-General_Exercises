a3474321303550d6a3b313c4d4d91d0a5a1bf678
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-18 03:06:12 +0000

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,8 +25,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 
-	for (len1 = 0; s1[len1]; len1++);
-	for (len2 = 0; s2[len2] && len2 < n; len2++);
+	for (len1 = 0; s1[len1]; len1++)
+		;
+
+	for (len2 = 0; s2[len2] && len2 < n; len2++)
+		;
 
 	conc = malloc(sizeof(char) * (len1 + len2 + 1));
 
