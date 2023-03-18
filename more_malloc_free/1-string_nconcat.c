b8f77d5e0776c807625c6411ed4ffc999bf3cf76
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-18 02:41:21 +0000

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,23 +12,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	if (s1 == NULL) {
-	s1 = "";
-    }
-    if (s2 == NULL) {
-	s2 = "";
-    }
-    size_t s1_len = strlen(s1);
-    size_t s2_len = strlen(s2);
-    if (n >= s2_len) {
-	n = s2_len;
-    }
-    char *result = (char *) malloc(s1_len + n + 1);
-    if (result == NULL) {
-	return NULL;
-    }
-    strncpy(result, s1, s1_len);
-    strncpy(result + s1_len, s2, n);
-    result[s1_len + n] = '\0';
-    return result;
+	size_t s1_len = strlen(s1);
+	size_t s2_len = strlen(s2);
+	char *result;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	if (n >= s2_len)
+	{
+	    n = s2_len;
+	}
+
+	result = malloc(s1_len + n + 1);
+
+	if (result == NULL)
+	{
+	    return (NULL);
+	}
+
+	strncpy(result, s1, s1_len);
+	strncpy(result + s1_len, s2, n);
+	result[s1_len + n] = '\0';
+	return (result);
 }
