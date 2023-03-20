7027823121c43b4488813221310521ff6745bd9f
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-20 18:50:29 +0000

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,16 @@
-
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - new type structure
+ *@name: char
+ *@age: float
+ *@owner: char
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
