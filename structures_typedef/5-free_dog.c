95a50f782765e272ef4052df9f7da9e5c3c03402
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-22 21:19:54 +0000

    5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
  *free_dog - frees dog
@@ -7,6 +9,8 @@
 
 void free_dog(dog_t *d)
 {
+	if (d==NULL)
+		return;
 	free(d->owner);
 	free(d->name);
 	free(d);
