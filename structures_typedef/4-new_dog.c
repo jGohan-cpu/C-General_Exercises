4c7d361cd013403fbb5421060b1bdd1ccb89265e
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-22 21:11:58 +0000

    3

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,5 +10,35 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *ndog;
 
+	ndog = malloc(sizeof(struct dog));
+	if (ndog == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	ndog->name = malloc(strlen(name) + 1);
+	if (ndog->name == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
+	else
+		strcpy(ndog->name, name);
+
+	ndog->age = age;
+	ndog->owner = malloc(strlen(owner) + 1);
+	if (ndog->owner == NULL)
+	{
+		free(ndog->name);
+		free(ndog->owner);
+		free(ndog);
+		return (NULL);
+	}
+	else
+		strcpy(ndog->owner, owner);
+
+	return (ndog);
 }
