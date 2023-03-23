e4553d5567433f22ba5cafccaed4025cc06c2ebf
jGohan-cpu <jjoseriveram01@gmail.com>
2023-03-23 03:13:33 +0000

    0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - name printing function
+ * @name: pointer to name
+ * @f: function pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
