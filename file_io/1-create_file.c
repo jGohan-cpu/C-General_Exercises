9ce9b842f163c075959cc6df2ce241e389e99fc9
Jose Rivera <113808890+jGohan-cpu@users.noreply.github.com>
2023-04-12 09:59:11 -0400

    Update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int kip, len;
+	int kip, lenght;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,8 +21,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		len = strlen(text_content);
-		write(kip, text_content, len);
+		lenght = strlen(text_content);
+		write(kip, text_content, lenght);
 		close(kip);
 	}
 	return (1);
