bcdc3e05c153abe4ba3f897dfbd754d8a3d96b7e
jGohan-cpu <jjoseriveram01@gmail.com>
2023-05-25 20:12:04 +0000

    added linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,70 +1,29 @@
 #include "search_algos.h"
 /**
- * print_array - prints array
- * @array: sorted array of ints
- * @high: upper bound
- * @low: lower bound
- */
-void print_array(int *array, size_t high, size_t low)
-{
-	if (low > high)
-		return;
-	printf("Searching in array: ");
-	if (low == high)
-	{
-		printf("%u\n", array[high]);
-		return;
-	}
-
-	while (low <= high)
-	{
-		printf("%u", array[low]);
-		if (low != high)
-			printf(", ");
-		low++;
-	}
-	printf("\n");
-}
-/**
- * b_search - binary search helper function
- * Recursively searches for value based on midpoint
- * @array: sorted array of ints
- * @high: top end of array
- * @low: lower bound of array
- * @value: value to search for
- * Return: postion or -1 if not found
- */
-int b_search(int *array, size_t high, size_t low, int value)
-{
-	int mid = low + (high - low) / 2;
-
-	print_array(array, high, low);
-
-	if (array[mid] == value)
-		return (mid);
-	if (high >= low)
-	{
-		if (array[mid] > value)
-			return (b_search(array, mid - 1, low, value));
-		else
-			return (b_search(array, high, mid + 1, value));
-	}
-	return (-1);
-}
-/**
- * binary_search - binary search algorithm
- * @array: array of sorted ints
+ * linear_search - linear search algorithm
+ * @array: array of ints
  * @size: size of array
- * @value: value to serach for
+ * @value: value to search for
  * Return: position or -1 if not found
+ *
  */
-int binary_search(int *array, size_t size, int value)
+int linear_search(int *array, size_t size, int value)
 {
-	int ret;
+	int i = 0;
 
 	if (array == NULL)
 		return (-1);
 
-	ret = b_search(array, (int)size - 1, 0, value);
-	return (ret);
+	while (i < (int)size)
+	{
+		if (array[i] == value)
+		{
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+			return (i);
+		}
+		else
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		i++;
+	}
+	return (-1);
 }
